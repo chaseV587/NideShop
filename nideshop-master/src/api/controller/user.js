b87561b0cc163fd854869647524fbb9f32ba53b6
Chase <943616235@qq.com>
2018-08-23 15:33:55 +0800

    fix 修改登录用户保存方式

--- nideshop-master/src/api/controller/user.js
+++ nideshop-master/src/api/controller/user.js
@@ -3,26 +3,26 @@ const fs = require('fs');
 const _ = require('lodash');
 
 module.exports = class extends Base {
-  async infoAction() {
-    const userInfo = await this.model('user').where({mobile: '15989389319'}).find();
-    delete userInfo.password;
-    return this.json(userInfo);
-  }
-
-  /**
-   * 保存用户头像
-   * @returns {Promise.<void>}
-   */
-  async saveAvatarAction() {
-    const avatar = this.file('avatar');
-    if (think.isEmpty(avatar)) {
-      return this.fail('保存失败');
+    async infoAction() {
+        const userInfo = await this.model('user').where({id: this.getLoginUserId()}).find();
+        delete userInfo.password;
+        return this.json(userInfo);
     }
 
-    const avatarPath = think.RESOURCE_PATH + '/static/user/avatar/1.' + _.last(_.split(avatar.path, '.'));
+    /**
+     * 保存用户头像
+     * @returns {Promise.<void>}
+     */
+    async saveAvatarAction() {
+        const avatar = this.file('avatar');
+        if (think.isEmpty(avatar)) {
+            return this.fail('保存失败');
+        }
 
-    fs.rename(avatar.path, avatarPath, function(res) {
-      return this.success();
-    });
-  }
+        const avatarPath = think.RESOURCE_PATH + `/static/user/avatar/${this.getLoginUserId()}.` + _.last(_.split(avatar.path, '.'));
+
+        fs.rename(avatar.path, avatarPath, function(res) {
+            return this.success();
+        });
+    }
 };
